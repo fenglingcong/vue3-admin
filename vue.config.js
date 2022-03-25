903f55b99783ab0b474d22e44f971b0f17be2fc6
fenglingcong <1539027251@qq.com>
2022-03-25 18:08:55 +0800

    '用本地IP自动打开浏览器'

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,19 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  devServer: {
+    open: true,
+    host: 'local-ip',
+    client: {
+      // Can be `string`:
+      //
+      // To get protocol/hostname/port from browser
+      // webSocketURL: 'auto://0.0.0.0:0/ws'
+      webSocketURL: {
+        hostname: '0.0.0.0',
+        pathname: '/ws',
+        port: 8080,
+      },
+    },
+  },
 });
