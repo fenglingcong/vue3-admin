b630c47c357cd20d18c832b76f23f6d0ba0029c6
fenglingcong <1539027251@qq.com>
2022-04-02 18:06:46 +0800

    'layout'

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,24 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
+import BasicLayout from '@/layouts/BasicLayout.vue';
 
 const routes = [
   {
     path: '/login',
     name: 'login',
-    component: () => import(/* webpackChunkName: "login" */ '@/views/user/Login.vue'),
+    component: () => import(/* webpackChunkName: "login" */ '@/views/user/SLogin.vue'),
   },
   {
     path: '/',
-    name: 'home',
-    component: HomeView,
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
+    name: 'index',
+    redirect: '/home',
+    component: BasicLayout,
+    children: [
+      {
+        path: '/home',
+        name: 'home',
+        component: () => import(/* webpackChunkName: "home" */ '@/views/SHome.vue'),
+      },
+    ],
   },
 ];
 
